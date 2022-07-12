507c0592469b37f7062357d0d9f931ad16831192
alex karpovich <alieksandr.karpovich@mail.ru>
2022-07-12 14:11:41 +0300

    add components

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,13 +26,13 @@ const Home: NextPage = () => {
           <div className={styles.card}>
             <h4>Взрослый</h4>
             <Image src="/woman.png" alt="Vercel Logo" width={300} height={300} />
-            <div className={styles.card}>
+            <div className={styles.comments}>
               <p>Менее тяжелые формы СМА могут возникать и диагностироваться в зрелом возрасте. </p>
             </div>
-            <div className={styles.card}>
+            <div className={styles.comments}>
             <p>По сравнению с СМА у детей, СМА у взрослых может иметь более легкие симптомы, но без патогенетической терапии пациенты со СМА 2-3 типа неуклонно теряют двигательные навыки. </p>
             </div>
-            <div className={styles.card}>
+            <div className={styles.comments}>
               <p>По сравнению с СМА в детстве, течение СМА у взрослых может быть более коварным и трудным для распознавания.  </p>
             </div>
           </div>
@@ -40,10 +40,10 @@ const Home: NextPage = () => {
           <div className={styles.card}>
             <h4>Ребенок</h4>
             <Image src="/child.png" alt="Vercel Logo" width={300} height={300} />
-            <div className={styles.card}>
+            <div className={styles.comments}>
               <p>Ребенок, плохо удерживающий голову, когда ему придают сидячее положение, с вялыми движениями нижних конечностей или с трудом тянущийся к предметам, но при этом с осмысленным и ярким взглядом, улыбающийся и социально активный, вызывает настороженность в отношении наличия СМА.</p>
             </div>
-            <div className={styles.card}>
+            <div className={styles.comments}>
               <p>Младенцам со СМА необходимо экстренное направление к специалисту, ранняя диагностика и обеспечение терапией, спасающей жизнь, поскольку эти мотонейроны очень быстро подвергаются дегенерации при прогрессировании заболевания</p>
             </div>
           </div>
@@ -66,7 +66,7 @@ const Home: NextPage = () => {
             </p>
           </a> */}
         </div>
-        <p>Сообщает ли один из ваших пациентов о следующих симптомах? (нажмите на любую иконку и узнайте больше)</p>
+        <p className={styles.description}>Сообщает ли один из ваших пациентов о следующих симптомах? (нажмите на любую иконку и узнайте больше)</p>
       </main>
 
       {/* <footer className={styles.footer}>
@@ -81,6 +81,8 @@ const Home: NextPage = () => {
           </span>
         </a>
       </footer> */}
+
+      
     </div>
   )
 }
